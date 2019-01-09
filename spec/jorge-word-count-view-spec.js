c21c21ea229f6015d87d51e101ec1abc81b511d0
JorgeCV <JorgeCV@ibm.com>
2019-01-09 09:43:09 -0800

    first commit

---
+++ spec/jorge-word-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JorgeWordCountView from '../lib/jorge-word-count-view';
+
+describe('JorgeWordCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
