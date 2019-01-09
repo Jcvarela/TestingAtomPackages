75b45065989395dff48e6cc5e8d1e350f3ddee29
JorgeCV <JorgeCV@ibm.com>
2019-01-09 10:59:19 -0800

    testing

--- spec/jorge-word-count-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import JorgeWordCountView from '../lib/jorge-word-count-view';
-
-describe('JorgeWordCountView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
